2ac0fd8ffbb956cf0a6833890cb2c72922bbdf5e
Sudarshan P <sudarshanpadekal2006@gmail.com>
2026-09-10 21:18:34 +0530

    5TH COMMIT

--- include/server.h
+++ include/server.h
@@ -1,15 +1,6 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "common.h"
-
-/*
- * Starts the Intellicompress HTTP server.
- *
- * Returns:
- *  0  on successful shutdown
- * -1  on initialization failure
- */
-int start_server(int port);
+int start_http_server(int port);
 
 #endif
